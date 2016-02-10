507ffda5ef170365216d54778529b18c71632dd9
ruslan <ruslanarshinnik@gmail.com>
2016-02-10 23:11:15 +0200

    urla test

---
+++ js/main.js
@@ -0,0 +1,14 @@
+    $(document).ready(function() {
+     
+      $(".owl-carousel").owlCarousel({
+      	items :1,
+      	itemsDesktop : [1199,1],
+      	itemsDesktopSmall : [980,1],
+      	itemsTablet: [768,1],
+      	navigation: true,
+      	pagination: false,
+      	navigationText : ["",""]
+      });
+
+     
+    });
